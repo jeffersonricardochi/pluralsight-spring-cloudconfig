40e8cbe73be7023930fe6e9edbe69de3d90ce8de
jeffersonricardochi <jeffersonricardochi@gmail.com>
2022-02-14 16:27:51 +0100

    return last code git config

---
+++ src/main/java/com/pluralsight/configservergit/ConfigserverGitApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.configservergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverGitApplication.class, args);
+	}
+
+}
